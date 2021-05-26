347e76ffd5d284314642662c6e9c719324e0e1c3
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-05-26 14:36:25 -0500

    Remove the standalone ElementNameAlignment class (nearly unused)

--- core/AttributeAlignment.cs
+++ core/AttributeAlignment.cs
@@ -219,37 +219,71 @@ namespace PrettyRegistryXml.Core
         #endregion
     }
 
-    public static class ElementNameAlignment
+    /// <summary>
+    /// Aligment for an entire element: element name and attributes.
+    /// </summary>
+    /// <remarks>
+    /// Typically created with <see cref="ElementAlignment.FindElementAlignment(IEnumerable{XElement}, IDictionary{AttributeName, int}?)"/>.
+    /// Wraps an integer for element name alignment, and an array of <see cref="AttributeAlignment"/>
+    /// </remarks>
+    public struct ElementAlignment
     {
+        /// <summary>
+        /// Alignment for the element name
+        /// </summary>
+        public int NameAlignment { get; init; }
+
+        /// <summary>
+        /// An array of alignments for attributes.
+        /// </summary>
+        public AttributeAlignment[] AttributeAlignments { get; init; }
 
         #region XElement-related methods
+
+        /// <summary>
+        /// Find the largest element name length
+        /// </summary>
+        /// <param name="elements">A collection of elements</param>
+        /// <returns>Maximum width/alignment</returns>
         public static int FindNameAlignment(IEnumerable<XElement> elements)
         {
             return (from el in elements
                     let len = el.Name.LocalName.Length
                     select len).Max();
         }
-        #endregion
 
-        #region Other helpers
-        public static void AppendNamePadding(int nameAlignment, XElement element, StringBuilder stringBuilder)
+        /// <summary>
+        /// Compute an ElementAlignment for a collection of elements.
+        /// </summary>
+        /// <remarks>
+        /// This wraps both <see cref="AttributeAlignment.FindAttributeAlignments(IEnumerable{XElement}, IDictionary{AttributeName, int})" />
+        /// and <see cref="ElementAlignment.FindElementAlignment(IEnumerable{XElement}, IDictionary{AttributeName, int})" />
+        /// </remarks>
+        /// <param name="elements">A collection of elements</param>
+        /// <param name="extraWidth">Optional dictionary of attribute name to additional width</param>
+        /// <returns>Array of alignments</returns>
+        public static ElementAlignment FindElementAlignment(IEnumerable<XElement> elements, IDictionary<AttributeName, int>? extraWidth = null)
         {
-            var len = element.Name.LocalName.Length;
-            if (len < nameAlignment)
+            var elts = elements.ToArray();
+            var nameAlignment = FindNameAlignment(elts);
+            var attrAligns = AttributeAlignment.FindAttributeAlignments(elts, extraWidth);
+            return new ElementAlignment
             {
-                stringBuilder.Append("".PadRight(nameAlignment - len));
-            }
+                NameAlignment = nameAlignment,
+                AttributeAlignments = attrAligns,
+            };
         }
 
         #endregion
-    }
 
-    public struct ElementAlignment
-    {
-        public int NameAlignment;
 
-        public AttributeAlignment[] AttributeAlignments;
         #region Other helpers
+
+        /// <summary>
+        /// Append padding to a StringBuilder as appropriate for an element name.
+        /// </summary>
+        /// <param name="element">An element whose name has been written already</param>
+        /// <param name="stringBuilder">Where to append the spaces, if required.</param>
         public void AppendElementNamePadding(XElement element, StringBuilder stringBuilder)
         {
             var len = element.Name.LocalName.Length;
@@ -258,27 +292,8 @@ namespace PrettyRegistryXml.Core
                 stringBuilder.Append("".PadRight(NameAlignment - len));
             }
         }
+
         #endregion
 
-        #region XElement-related methods
-        /// <summary>
-        /// Compute an ElementAlignment for a collection of elements.
-        /// This wraps both FindNameAlignment and FindAttributeAlignments.
-        /// </summary>
-        /// <param name="elements">A collection of elements</param>
-        /// <param name="extraWidth">Optional dictionary of attribute name to additional width</param>
-        /// <returns>Array of alignments</returns>
-        public static ElementAlignment FindElementAlignment(IEnumerable<XElement> elements, IDictionary<AttributeName, int> extraWidth = null)
-        {
-            var elts = elements.ToArray();
-            var nameAlignment = ElementNameAlignment.FindNameAlignment(elts);
-            var attrAligns = AttributeAlignment.FindAttributeAlignments(elts, extraWidth);
-            return new ElementAlignment
-            {
-                NameAlignment = nameAlignment,
-                AttributeAlignments = attrAligns,
-            };
-        }
-        #endregion
     }
 }
