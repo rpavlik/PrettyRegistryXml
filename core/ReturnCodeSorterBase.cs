97bf277fbe71cc9b4a237c342da8770ed23d7326
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-06-03 11:05:31 -0500

    Factor out base of return code sorting too.

---
+++ core/ReturnCodeSorterBase.cs
@@ -0,0 +1,54 @@
+// Copyright 2021 Collabora, Ltd
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace PrettyRegistryXml.Core
+{
+    /// <summary>
+    /// Base utility class for sorting return codes, with some provided codes always in a given order,
+    /// and the rest alphabetical after that.
+    /// </summary>
+    public class ReturnCodeSorterBase
+    {
+        private readonly Dictionary<string, Tuple<string, int>> importance;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="presortedSpecialItems">Your "special" codes, in the order you want them to appear.</param>
+        public ReturnCodeSorterBase(string[] presortedSpecialItems)
+            => importance = presortedSpecialItems
+                                // reverse so that later items get a smaller index
+                                .Reverse()
+                                // turn items into an item, increased-reverse-index tuple
+                                .Select((item, index) => (item, index + 100).ToTuple())
+                                .ToDictionary(tup => tup.Item1, tup => tup);
+
+
+        /// <summary>
+        /// Sorts a string of comma-separated return codes.
+        /// </summary>
+        /// <remarks>
+        /// A few common, generic codes are sorted explicitly to the front of the list,
+        /// with all remaining codes alphabetical after them.
+        /// </remarks>
+        /// <param name="vals">comma-separated return codes</param>
+        /// <returns>comma-separated return codes in the desired order</returns>
+        public string SortReturnCodeString(string vals)
+            => string.Join(',', vals.Split(',', StringSplitOptions.RemoveEmptyEntries).OrderByDescending(GetKey));
+        private Tuple<string, int> GetKey(string item)
+        {
+            if (importance.TryGetValue(item, out var ret))
+            {
+                return ret;
+            }
+            return (item, 0).ToTuple();
+        }
+    }
+}
--- openxr/ReturnCodeSorter.cs
+++ openxr/ReturnCodeSorter.cs
@@ -4,18 +4,16 @@
 
 #nullable enable
 
-using System.Collections.Generic;
-using System.Linq;
-using System;
+using PrettyRegistryXml.Core;
 
 namespace PrettyRegistryXml.OpenXR
 {
     /// <summary>
     /// A utility class for OpenXR's policy for sorting return codes.
     /// </summary>
-    public sealed class ReturnCodeSorter
+    public sealed class ReturnCodeSorter : ReturnCodeSorterBase
     {
-        private readonly Dictionary<string, Tuple<string, int>> importance = new string[]{
+        public ReturnCodeSorter() : base(new string[]{
                 // These codes will be sorted first, in this order.
                 "XR_SUCCESS",
                 "XR_SESSION_LOSS_PENDING",
@@ -28,31 +26,7 @@ namespace PrettyRegistryXml.OpenXR
                 "XR_ERROR_OUT_OF_MEMORY",
                 "XR_ERROR_LIMIT_REACHED",
                 "XR_ERROR_SIZE_INSUFFICIENT",
-            }
-            // reverse so that later items get a smaller index
-            .Reverse()
-            .Select((item, index) => (item, index + 100).ToTuple())
-            .ToDictionary(tup => tup.Item1, tup => tup);
-
-
-        private Tuple<string, int> GetKey(string item)
-        {
-            if (!importance.TryGetValue(item, out var ret))
-            {
-                ret = (item, 0).ToTuple();
-            }
-            return ret;
-        }
-
-        /// <summary>
-        /// Sorts a string of comma-separated return codes.
-        /// </summary>
-        /// <remarks>
-        /// A few common, generic codes are sorted explicitly to the front of the list, with all remaining codes alphabetical after them.
-        /// </remarks>
-        /// <param name="vals">comma-separated return codes</param>
-        /// <returns>comma-separated return codes in the desired order</returns>
-        public string SortReturnCodeString(string vals)
-            => string.Join(',', vals.Split(',', StringSplitOptions.RemoveEmptyEntries).OrderByDescending(GetKey));
+            })
+        { }
     }
 }
