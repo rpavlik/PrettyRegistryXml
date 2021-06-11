68150222640970ae259d381fafb2d3b401eb88c7
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-06-11 16:25:09 -0500

    Move the guts of the artifact building to a script.

---
+++ Build-Artifacts.ps1
@@ -0,0 +1,27 @@
+# Copyright 2021 Collabora, Ltd
+#
+# SPDX-License-Identifier: MIT
+
+if (Test-Path Env:BUILD_OS) {} else {
+    $Env:BUILD_OS="ubuntu-latest"
+}
+$Env:build_version = (git describe).Trim("v")
+if (Test-Path Env:BUILD_REF) {
+    if ("$Env:BUILD_REF".StartsWith("refs/tags/v")) {
+        $Env:build_version = "$Env:BUILD_REF".Trim("refs/tags/v")
+    }
+}
+
+$Version = $Env:build_version
+$ArtifactName="PrettyRegistryXml-${Env:BUILD_OS}-$Version"
+
+if (Test-Path Env:GITHUB_ENV)
+{
+    # Provide these variables to the following actions as well
+    Write-Output "build_version=$Env:build_version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+    Write-Output "ARTIFACT_NAME=$ArtifactName" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+}
+
+dotnet build -c Release "-property:Version=$Version" "-property:AssemblyFileVersion=$Version" "-property:AssemblyInformationalVersionVersion=$Version"
+dotnet publish -c Release -o $ArtifactName/ "-property:Version=$Version"  "-property:AssemblyFileVersion=$Version" "-property:AssemblyInformationalVersionVersion=$Version"
+7z a -bd -r out/$ArtifactName.7z $ArtifactName/
