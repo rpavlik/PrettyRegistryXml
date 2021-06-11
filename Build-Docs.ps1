dfd4c968e5880fd8baa7119ebcd74b7e899e9184
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-06-11 17:10:32 -0500

    Rename build-docs for consistency.

--- Build-Docs.ps1
+++ Build-Docs.ps1
@@ -6,6 +6,5 @@
 # dotnet clean
 dotnet build
 
-$repoBase = "https://github.com/rpavlik/PrettyRegistryXml"
 dotnet tool run xmldocmd core/bin/Debug/net5.0/PrettyRegistryXml.Core.dll docs --source ../core --clean
 dotnet tool run xmldocmd openxr/bin/Debug/net5.0/PrettyRegistryXml.OpenXR.dll docs --source ../openxr --clean
