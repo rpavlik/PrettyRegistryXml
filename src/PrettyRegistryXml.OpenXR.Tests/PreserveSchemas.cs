e7d4590168344e5526568ede5c08f88a49d9bd26
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-08-11 10:55:06 -0500

    Factor out some utils for data-driven tests

--- src/PrettyRegistryXml.OpenXR.Tests/PreserveSchemas.cs
+++ src/PrettyRegistryXml.OpenXR.Tests/PreserveSchemas.cs
@@ -4,10 +4,8 @@
 
 #nullable enable
 
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using Xunit;
 
 
@@ -15,16 +13,6 @@ namespace PrettyRegistryXml.OpenXR.Tests
 {
     public class PreserveSchemasTest
     {
-        private static string GetTestFileDir()
-        {
-            var assemblyPath = Uri.UnescapeDataString(new Uri(Assembly.GetExecutingAssembly().Location).AbsolutePath);
-            return Path.Join(Path.GetDirectoryName(assemblyPath), "TestFiles");
-        }
-        private static string NormalizeText(string s)
-        {
-            return s.ReplaceLineEndings().Trim();
-        }
-
         public static IEnumerable<object[]> RoundTripFilenames => new List<object[]>{
             new object[] {
                 "sample.xml"
@@ -34,7 +22,7 @@ namespace PrettyRegistryXml.OpenXR.Tests
         [MemberData(nameof(RoundTripFilenames))]
         public void RoundtripData(string filename)
         {
-            var fullPath = Path.Join(GetTestFileDir(), filename);
+            var fullPath = Path.Join(TestDataUtils.GetTestFileDir(), filename);
             var outFullPath = fullPath + ".out";
 
             var opts = new Options
@@ -47,7 +35,7 @@ namespace PrettyRegistryXml.OpenXR.Tests
             Program.Run(opts);
             var orig = File.ReadAllText(fullPath);
             var formatted = File.ReadAllText(outFullPath);
-            Assert.Equal(NormalizeText(orig), NormalizeText(formatted));
+            Assert.Equal(TestDataUtils.NormalizeText(orig), TestDataUtils.NormalizeText(formatted));
         }
 
     }
